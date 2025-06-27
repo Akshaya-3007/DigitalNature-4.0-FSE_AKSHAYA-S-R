7a1ec43d3165fcda5113265fb8ab2f49dbdd788f
Akshaya-3007 <akshayasrpec@gmail.com>
2025-06-27 11:21:14 +0530

    Update CalculatorTest.java

--- WEEK2/JUnit,Mockito and SL4J/1. JUnit_Basic Testing Exercises/Exercise 4: Arrange-Act-Assert (AAA) Pattern, Test Fixtures, Setup and Teardown Methods in JUnit/CalculatorTest.java
+++ WEEK2/JUnit,Mockito and SL4J/1. JUnit_Basic Testing Exercises/Exercise 4: Arrange-Act-Assert (AAA) Pattern, Test Fixtures, Setup and Teardown Methods in JUnit/CalculatorTest.java
@@ -11,34 +11,34 @@ public class CalculatorTest {
 
     @Before
     public void setUp() {
-        // Setup: Called before each test
+        
         calc = new Calculator();
         System.out.println("Setup completed");
     }
 
     @After
     public void tearDown() {
-        // Teardown: Called after each test
+        
         calc = null;
         System.out.println("Teardown completed");
     }
 
     @Test
     public void testAdd() {
-        // Arrange
+       
         int a = 10;
         int b = 5;
 
-        // Act
+       
         int result = calc.add(a, b);
 
-        // Assert
+       
         assertEquals(15, result);
     }
 
     @Test
     public void testSubtract() {
-        // Arrange
+        
         int a = 10;
         int b = 3;
 
